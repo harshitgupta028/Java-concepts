fa473d80c769f6ee7f188916b2774510a911321b
harshitg123 <harshit.28.sps@gmail.com>
2024-01-19 22:40:07 +0530

    Java practice

--- Questions/fabonnachi.java
+++ Questions/fabonnachi.java
@@ -7,6 +7,20 @@ import java.util.Scanner;
  */
 public class fabonnachi {
 
+    static int nextElem;
+
+    static void printFabonachiSeries(int count, int firstElem, int secondElem){
+
+        if (count > 0) {
+            nextElem = firstElem + secondElem;
+            System.out.println(nextElem);
+            firstElem = secondElem;
+            secondElem = nextElem;
+            printFabonachiSeries(count - 1, firstElem, secondElem);
+            
+        }
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -15,10 +29,13 @@ public class fabonnachi {
         int count = sc.nextInt();
 
         sc.close();
-        
+
         int firstElem = 0;
         int secondElem = 1;
 
+        
+        if (count >= 3) {
+           
         System.out.println(firstElem);
         System.out.println(secondElem);
 
@@ -34,5 +51,19 @@ public class fabonnachi {
             secondElem = nextNo;
             
         }
+
+        // Using recursion.
+        System.out.println(count);
+        firstElem = 0;
+        secondElem = 1;
+        System.out.println(firstElem);
+        System.out.println(secondElem);
+        printFabonachiSeries(count, firstElem, secondElem);
+        }else{
+            System.out.println(firstElem);
+            System.out.println(secondElem);
+    
+        }
+        
     }
 }
