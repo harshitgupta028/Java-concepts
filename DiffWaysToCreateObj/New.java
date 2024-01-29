ac58c1bdb02d639747195a99d1711ea20b1a7de0
harshitg123 <harshit.28.sps@gmail.com>
2024-01-29 23:02:41 +0530

    comment for new

--- DiffWaysToCreateObj/New.java
+++ DiffWaysToCreateObj/New.java
@@ -6,6 +6,9 @@ public class New {
     public static void main(String[] args) {
         
         // Here object is created using new keyword
+
+        // using new keyword to create the object is popular and it allocates the memory for the object
+        // and also returns the reference to that object to that memory
         Sample sampleObj = new Sample();
     }
 }
