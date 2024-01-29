464e65e599a06822c41f74766fb890d0f7cafeca
harshitg123 <harshit.28.sps@gmail.com>
2024-01-29 23:00:30 +0530

    using new to create object

---
+++ DiffWaysToCreateObj/New.java
@@ -0,0 +1,11 @@
+package DiffWaysToCreateObj;
+
+import DiffWaysToCreateObj.Sample.Sample;
+
+public class New {
+    public static void main(String[] args) {
+        
+        // Here object is created using new keyword
+        Sample sampleObj = new Sample();
+    }
+}
---
+++ DiffWaysToCreateObj/Sample/Sample.java
@@ -0,0 +1,9 @@
+package DiffWaysToCreateObj.Sample;
+
+public class Sample {
+    
+    private String name;
+    private int id;
+
+    
+}
