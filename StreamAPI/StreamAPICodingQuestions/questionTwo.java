4f614aabc9153ffabaec6c8a4155f6b06223f4a7
harshitg123 <harshit.28.sps@gmail.com>
2025-01-24 20:59:58 +0530

    Question 2

---
+++ StreamAPI/StreamAPICodingQuestions/questionTwo.java
@@ -0,0 +1,22 @@
+package StreamAPICodingQuestions;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class questionTwo {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = List.of(12, 43, 123, 55, 45, 10);
+
+        // One
+        List<Integer> transformedData = list.stream().map(n -> n + "")
+                .filter(n -> n.startsWith("1"))
+                .map(n -> Integer.parseInt(n))
+                .collect(Collectors.toList());
+        System.out.println(transformedData);
+
+        // Two
+        list.stream().map(n -> n + "").filter(n -> n.startsWith("1")).forEach(n -> System.out.print(n + " "));
+    }
+}
